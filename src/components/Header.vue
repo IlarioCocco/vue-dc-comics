220815cf8ba347ff5b01d2c03c6607fe1d23d046
IlarioCOCCO <ilario.cocco@gmail.com>
2021-10-01 09:52:36 +0200

    reset commit

---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="Header">
+      <div>
+          <img src="" alt="">
+      </div>
+  </div>
+</template>
+
+
+
+
+
+
+<script>
+export default {
+  name: 'Header',
+ 
+}
+</script>
+
+
+
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.Header {
+    text-align: center;
+    height: 60px;
+    background: rgb(201, 201, 201);
+}
+
+
+
+</style>
